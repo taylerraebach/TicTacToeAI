052fce2eb4f402f5175963a5dae473cfdcde0f78
taylerraebach <taylerraebach@gmail.com>
2025-01-28 18:09:37 -0500

    user turn fully operational, issues with ai turn

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ font = pygame.font.Font(None, 200)
 screen = pygame.display.set_mode((WINDOW_SIZE, WINDOW_SIZE))
 pygame.display.set_caption("AI TicTacToe")
 
-# drawing the lines for the tuc tac toe board
+
+#-----------------------------------------------------------------------------------------------------------------------
 def drawBoard():
     # vertical lines
     pygame.draw.line(screen, BLACK, (WINDOW_SIZE / 3, 0), (WINDOW_SIZE / 3, WINDOW_SIZE), SHAPE_WIDTH)
@@ -26,6 +27,7 @@ def drawBoard():
     pygame.draw.line(screen, BLACK, (0, (WINDOW_SIZE / 3) * 2), (WINDOW_SIZE, (WINDOW_SIZE / 3) * 2), SHAPE_WIDTH)
 
 
+#-----------------------------------------------------------------------------------------------------------------------
 def squareLocation(squareNum):
     # determining based on which square the user/ai selected, where the center of the x/o will be
     # board looks like:
@@ -34,35 +36,97 @@ def squareLocation(squareNum):
     # 6 7 8
     if squareNum == 0:
         return (WINDOW_SIZE / 6, WINDOW_SIZE / 6)
+
     elif squareNum == 1:
         return (WINDOW_SIZE / 2, WINDOW_SIZE / 6)
+
     elif squareNum == 2:
         return ((WINDOW_SIZE / 6) * 5, WINDOW_SIZE / 6)
+
     elif squareNum == 3:
         return (WINDOW_SIZE / 6, WINDOW_SIZE / 2)
+
     elif squareNum == 4:
         return (WINDOW_SIZE / 2, WINDOW_SIZE / 2)
+
     elif squareNum == 5:
         return ((WINDOW_SIZE / 6) * 5, WINDOW_SIZE / 2)
+
     elif squareNum == 6:
         return (WINDOW_SIZE / 6, (WINDOW_SIZE / 6) * 5)
+
     elif squareNum == 7:
         return (WINDOW_SIZE / 2, (WINDOW_SIZE / 6) * 5)
+
     elif squareNum == 8:
         return ((WINDOW_SIZE / 6) * 5, (WINDOW_SIZE / 6) * 5)
 
 
-def drawO(squareNum):
+#-----------------------------------------------------------------------------------------------------------------------
+def decideSquareNum(x, y):
+    # deciding based on click position, which square is it in
+    # board looks like:
+    # 0 1 2
+    # 3 4 5
+    # 6 7 8
+
+    # if top row
+    if(y < (WINDOW_SIZE / 3)):
+        # if left column
+        if (x < (WINDOW_SIZE / 3) ):
+            return 0
+
+        # if middle column
+        elif (x < (WINDOW_SIZE / 3) * 2):
+            return 1
+
+        # if right column
+        else:
+            return 2
+
+    # if middle row
+    elif(y < ((WINDOW_SIZE / 3))*2):
+        # if left column
+        if (x < (WINDOW_SIZE / 3)):
+            return 3
+
+        # if middle column
+        elif (x < (WINDOW_SIZE / 3) * 2):
+            return 4
+
+        # if right column
+        else:
+            return 5
+
+    # if bottom row
+    else:
+        # if left column
+        if (x < (WINDOW_SIZE / 3)):
+            return 6
+
+        # if middle column
+        elif (x < (WINDOW_SIZE / 3) * 2):
+            return 7
+
+        # if right column
+        else:
+            return 8
+
+
+#-----------------------------------------------------------------------------------------------------------------------
+def drawO(aiMove):
     # Render the text (create a surface with the letter "X")
     text = font.render('O', True, BLACK)
 
     # Get the rectangle of the text and center it
-    text_rect = text.get_rect(center=(squareLocation(squareNum)))
+    location = squareLocation(aiMove)
+    text_rect = text.get_rect(center=location)
 
     # Draw the text on the screen
     screen.blit(text, text_rect)
 
 
+#-----------------------------------------------------------------------------------------------------------------------
 def drawX(squareNum):
     # Render the text (create a surface with the letter "X")
     text = font.render('X', True, BLACK)
@@ -74,6 +138,28 @@ def drawX(squareNum):
     screen.blit(text, text_rect)
 
 
+#-----------------------------------------------------------------------------------------------------------------------
+def AImoveDecision(listX, listO):
+
+    for i in range(len(listX)):
+        for j in range(len(listO)):
+            for k in range(7):
+                if((listX[i] != k) and (listO[j] != k)):
+                    return k
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+# so the board is not created every loop
+boardCreated = False
+
+# decides who's turn it is
+playerTurn = True
+AITurn = False
+
+# arrays that keep track of plays, entries are the squareNum
+listX = []
+listO = []
+
 # Game loop
 running = True
 while running:
@@ -81,13 +167,32 @@ while running:
         if event.type == pygame.QUIT:  # Check for window close event
             running = False
 
-    # Fill the screen with white
-    screen.fill(WHITE)
+    if (boardCreated == False):
+        # Fill the screen with white
+        screen.fill(WHITE)
+        # drawing game board lines
+        drawBoard()
+        boardCreated = True
 
-    # drawing game board lines
-    drawBoard()
-    drawO(4)
-    drawX(1)
+    if (playerTurn):
+        # Detect mouse button clicks
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:  # Left mouse button
+                x, y = event.pos  # Get the position of the mouse
+
+            drawX(decideSquareNum(x, y))
+            listX.append(decideSquareNum(x, y))
+            playerTurn = False
+            AITurn = True
+
+    if (AITurn):
+        # decide move
+        aiMove = AImoveDecision(listX, listO)
+        drawO(aiMove)
+        listO.append(aiMove)
+        AITurn = False
+        playerTurn = True
+
 
     pygame.display.update()
 
